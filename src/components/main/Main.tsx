bf4904226c402d7e62c994efc64135efa1039ab8
lulla-by <thfl3363@hanmail.net>
2024-03-03 22:10:35 +0900

    [Style] 포켓몬 리스트 관련 컴포넌트 생성

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import PokemonList from './PokemonList';
+
+const Main = () => {
+  return (
+    <Container>
+      <Title>포켓몬 도감</Title>
+      <PokemonList />
+    </Container>
+  );
+};
+
+export default Main;
+
+const Container = styled.main`
+  font-family: 'DungGeunMo';
+  margin: auto;
+  width: 800px;
+  background-color: lightblue;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  text-align: center;
+  padding: 5px;
+`;
