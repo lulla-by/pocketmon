800e8897a6a0dec28ed9c3cf923eee746252c62b
lulla-by <thfl3363@hanmail.net>
2024-03-03 23:54:38 +0900

    [Feat] 포켓몬 카드 렌더링

--- src/components/main/PokemonCard.tsx
+++ src/components/main/PokemonCard.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Pokemon } from '../../type/type';
+import { getPocketmontInfo } from '../../api';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-const PokemonCard = () => {
-  return (
-    <div>PokemonCard</div>
-  )
+interface PokemonCardProps {
+  monster: Pokemon;
 }
 
-export default PokemonCard
+const PokemonCard = ({ monster }: PokemonCardProps) => {
+  const navigate = useNavigate();
+  const id = monster.url.split('/')[6];
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+  const [data, setData] = useState<any>();
+  const fetchData = async () => {
+    const result = await getPocketmontInfo(id);
+    setData(result);
+  };
+
+  const moveDetail = () => {
+    navigate(`/detail/${id}`);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Monster onClick={moveDetail}>
+        {data && data.names[2].name}
+        <img src={img} alt="" width={100} height={100} />
+      </Monster>
+    </Container>
+  );
+};
+
+export default PokemonCard;
+
+const Container = styled.li`
+  padding: 10px;
+  border: 1px solid black;
+  position: relative;
+`;
+const Monster = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
