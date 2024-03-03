1d846e6769822d2b6825d205e9be9e2297e5801c
lulla-by <thfl3363@hanmail.net>
2024-03-03 18:11:00 +0900

    [Style] Description 컴포넌트 생성 및 CSS

---
+++ src/components/Description.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const Description = () => {
+
+  return (
+    <Container>Description</Container>
+  )
+}
+
+export default Description
+
+
+const Container =  styled.div`
+    width: 300px;
+    height: 400px;
+    background-color: black;
+    margin:auto;
+    color:#37b24d;
+    font-weight: 600;
+    border-radius: 5px;
+    
+`
--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import styled from 'styled-components';
+import Description from './Description';
 
 const Right = () => {
   return (
-    <Container>Right</Container>
+    <Container><Description/></Container>
   )
 }
 
 export default Right
 const Container = styled.div`
   width: 50%;
+  margin-top: 92px;
   background-color: #e03131;
-  border-radius: 5px;
+  border-radius: 0px 5px 5px 0px;
+  border-left: 1px solid black;
+  display: flex;
 `;
