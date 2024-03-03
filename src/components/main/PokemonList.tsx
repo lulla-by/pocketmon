62314a6166e3f0403e03088f42719f2723b0e5b6
lulla-by <thfl3363@hanmail.net>
2024-03-03 23:54:18 +0900

    [Feat]  포켓몬 리스트 렌더링

--- src/components/main/PokemonList.tsx
+++ src/components/main/PokemonList.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import PokemonCard from './PokemonCard';
+import { getPocketmontData } from '../../api';
+import { Pokemon } from '../../type/type';
+import styled from 'styled-components';
 
 const PokemonList = () => {
+  const [list, setList] = useState<Pokemon[]>();
+  const fetchData = async () => {
+    const result = await getPocketmontData();
+    setList(result);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div>PokemonList</div>
-  )
-}
+    <Container>
+      {list &&
+        list.map((monster: Pokemon) => <PokemonCard monster={monster} />)}
+    </Container>
+  );
+};
 
-export default PokemonList
+export default PokemonList;
+
+const Container = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 25px;
+`;
