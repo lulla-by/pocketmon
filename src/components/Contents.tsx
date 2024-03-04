5b4c770ff7743ab0843db47c0cb0f3fdbc20163a
lulla-by <thfl3363@hanmail.net>
2024-03-04 12:39:34 +0900

    [Refactor] 데이터 동적 처리

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,13 +1,24 @@
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Bg from '../assets/6114010.jpg';
 
-const Contents = ({data}:any) => {
+const Contents = ({ data }: any) => {
+  const returnName = (inputString: string) => {
+    const lastChar = inputString?.charAt(inputString.length - 1);
 
-  const imgSrc = ` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif`;
+    const hasConsonant = (lastChar?.charCodeAt(0) - 44032) % 28 > 0;
+    return hasConsonant ? `${inputString}이` : `${inputString}가`;
+  };
+
+  const params = useParams();
+  const id = params.id && params.id;
+  const name = returnName(data && data.names[2].name);
+
+  const imgSrc = ` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`;
   return (
     <Container>
-      <img src={imgSrc} width={100} height={100} alt=''/>
-      <p>앗 야생의 이상해씨가 나타났다!</p>
+      <img src={imgSrc} width={100} height={100} alt={name} />
+      <p>앗 야생의 {name} 나타났다!</p>
     </Container>
   );
 };
