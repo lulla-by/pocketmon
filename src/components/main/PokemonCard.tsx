022d148e0fa67bdcf935663567e1f6be2bfda706
lulla-by <thfl3363@hanmail.net>
2024-03-04 12:33:44 +0900

    [Style] 포켓몬 IdBox 배경색상 동적으로 적용

--- src/components/main/PokemonCard.tsx
+++ src/components/main/PokemonCard.tsx
@@ -8,6 +8,10 @@ interface PokemonCardProps {
   monster: Pokemon;
 }
 
+interface StyleProps {
+  color: string;
+}
+
 const PokemonCard = ({ monster }: PokemonCardProps) => {
   const navigate = useNavigate();
   const id = monster.url.split('/')[6];
@@ -18,6 +22,37 @@ const PokemonCard = ({ monster }: PokemonCardProps) => {
     const result = await getPocketmontInfo(id);
     setData(result);
   };
+
+  const name = data && data.names[2].name;
+  const color = getPastelToneColor(data && data.color.name);
+
+  function getPastelToneColor(name: string) {
+    switch (name?.toLowerCase()) {
+      case "black":
+          return "#adb5bd"; // 검은색
+      case "blue":
+          return "#4dabf7"; // 파란색
+      case "brown":
+          return "hsl(30, 50%, 40%)"; // 갈색
+      case "gray":
+          return "hsl(0, 0%, 50%)"; // 회색
+      case "green":
+          return "#8ce99a" // 초록색
+      case "pink":
+          return "#faa2c1"; // 분홍색
+      case "purple":
+          return "#da77f2"; // 보라색
+      case "red":
+          return "#fa5252"; // 빨간색
+      case "white":
+          return "hsl(0, 0%, 100%)"; // 흰색
+      case "yellow":
+          return "#ffd43b"; // 노란색
+      default:
+          return "#adb5bd(0, 0%, 50%)"; // 기본값 (회색)
+  }
+
+  }
 
   const moveDetail = () => {
     navigate(`/detail/${id}`);
@@ -30,7 +65,10 @@ const PokemonCard = ({ monster }: PokemonCardProps) => {
   return (
     <Container>
       <Monster onClick={moveDetail}>
-        {data && data.names[2].name}
+        <NameBox>
+          <IdBox color={color}>{id}</IdBox>
+          {name}
+        </NameBox>
         <img src={img} alt="" width={100} height={100} />
       </Monster>
     </Container>
@@ -45,9 +83,28 @@ const Container = styled.li`
   position: relative;
 `;
 const Monster = styled.div`
+  padding-top: 15px;
   margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+const NameBox = styled.div`
+  position: absolute;
+  left: 5px;
+  top: 5px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const IdBox = styled.p<StyleProps>`
+  background-color: ${({ color }) => color};
+  color: black;
+  width: 30px;
+  padding: 2px;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 1rem;
+  border: 1px solid black;
 `;
