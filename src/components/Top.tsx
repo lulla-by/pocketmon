c01aa51ad5fcd96d0985817cf84c685497f81121
lulla-by <thfl3363@hanmail.net>
2024-03-05 01:07:19 +0900

    [Bugfix] ButtonsStyleProps lowercase - boxShadow -> boxshadow - styled-component DOM issue

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -4,13 +4,19 @@ type ButtonStyleProps = {
   width: number;
   height: number;
   color: string;
+  boxshadow?: 'no';
 };
 
 const Top = () => {
   return (
     <Container>
       <Button width={60} height={60} color="#f1f3f5">
-        <Button width={50} height={50} color="#1c7ed6"></Button>
+        <Button
+          boxshadow={'no'}
+          width={50}
+          height={50}
+          color="#1c7ed6"
+        ></Button>
       </Button>
       <Button width={15} height={15} color="#e03131" />
       <Button width={15} height={15} color="#fcc419" />
@@ -21,17 +27,19 @@ const Top = () => {
 
 export default Top;
 const Container = styled.div`
-padding: 10px;
+  padding: 10px;
   display: flex;
   gap: 20px;
   border-bottom: 1px solid black;
-  padding-bottom:20px;
+  padding-bottom: 20px;
   margin-bottom: 20px;
 `;
 const Button = styled.div<ButtonStyleProps>`
   width: ${({ width }) => `${width}px`};
   height: ${({ height }) => `${height}px`};
   background: ${({ color }) => color};
+  box-shadow: ${({ boxshadow }) =>
+  boxshadow === 'no' ? 'none' : '2px 2px 3px black'};
   border-radius: 50%;
   border: 1px solid #495057;
   display: flex;
