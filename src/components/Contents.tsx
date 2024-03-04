0b21c71dcc8ec3996bdbbd32e9a88fb8677fdce2
lulla-by <thfl3363@hanmail.net>
2024-03-04 12:37:30 +0900

    [Style] Contents 마크업 및 CSS 완성

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,16 +1,31 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Bg from '../assets/6114010.jpg';
 
-const Contents = () => {
+const Contents = ({data}:any) => {
+
+  const imgSrc = ` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif`;
   return (
-    <Container>Contents</Container>
-  )
-}
+    <Container>
+      <img src={imgSrc} width={100} height={100} alt=''/>
+      <p>앗 야생의 이상해씨가 나타났다!</p>
+    </Container>
+  );
+};
 
-export default Contents
+export default Contents;
 
 const Container = styled.div`
-    height: 200px;
-    background-color: #343a40;
-    border-radius:5px;
+  height: 200px;
+  background: #343a40 url(${Bg}) no-repeat center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  gap: 15px;
 
-`
+  p {
+    font-size: 1.5rem;
+  }
+`;
