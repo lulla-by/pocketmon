63a9ffae7e590cfb1663ec9137f0fa065a4ec5a4
lulla-by <thfl3363@hanmail.net>
2024-03-04 12:57:23 +0900

    [Style] Description CSS 변경

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const Description = ({ data }: any) => {
-
   const des =
     data &&
     data.flavor_text_entries.filter(
@@ -15,11 +14,13 @@ const Description = ({ data }: any) => {
 
   return (
     <Container>
-      <Title>{data && data.names[2].name}({data?.genera[1].genus})</Title>
+      <Title>
+        {data && data.names[2].name}({data?.genera[1].genus})
+      </Title>
       <GroupList>
         {group &&
           group.map((group: any, i: number) => (
-            <MonsteDescription key={i}>{group.name}</MonsteDescription>
+            <GroupLi key={i}>{group.name}</GroupLi>
           ))}
       </GroupList>
       <ul>
@@ -37,12 +38,15 @@ export default Description;
 const Container = styled.div`
   width: 300px;
   height: 400px;
-  background-color: black;
+  background-color: white;
+  border: 6px solid black; /* 변경된 부분 */
   margin: auto;
-  color: #37b24d;
+  color: black;
+  border-style: double;
   font-weight: 600;
-  border-radius: 5px;
+  border-radius: 10px;
   padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 `;
 const Title = styled.p`
   font-size: 2rem;
@@ -52,6 +56,14 @@ const GroupList = styled.ul`
   gap: 20px;
 `;
 const MonsteDescription = styled.li`
+  margin-top: 20px;
+  font-size: 1.5rem;
+`;
+const GroupLi = styled.li`
+  background-color: black;
+  color: white;
+  padding: 2px 5px;
+  border-radius: 5px;
   margin-top: 20px;
   font-size: 1.5rem;
 `;
