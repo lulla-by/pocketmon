8582f2004328c69a74d48821ca01527ef0462a1a
lulla-by <thfl3363@hanmail.net>
2024-03-04 20:52:27 +0900

    [Refactor] props 및 API 통신관련 코드 제거

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -2,28 +2,14 @@ import Left from '../components/Left';
 import Right from '../components/Right';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { getPocketmontInfo } from '../api';
 
 const DetailPage = () => {
-  const params = useParams();
-  const id = params.id && params.id;
 
-  const fetchData = async () => {
-    const result = await getPocketmontInfo(id);
-    setData(result);
-  };
-
-  const [data, setData] = useState<any>();
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <Layout>
         <Container>
-          <Left data = {data} />
-          <Right data = {data} />
+          <Left />
+          <Right />
         </Container>
     </Layout>
   );
